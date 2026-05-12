80efba734b971efe402d7bea73be01ca33b14ed0
weixiang.liu <weixiang.liu@mijahealth.com>
2026-05-12 19:36:54 +0800

    feat(erii-plugins): 新增多个插件包及其安装脚本和文档
    - 新增 animal 插件包及 postinstall.js 脚本，实现插件zip文件符号链接功能
    - 新增 lolisuki 插件包，包含postinstall.js脚本和简单插件介绍
    - 新增 net-ease-music 插件包及其安装脚本支持网易云音乐插件管理
    - 新增 rollpig 插件包，附带安装脚本，实现zip文件链接到plugins目录
    - 新增 seeddream 插件包，含安装脚本，支持图生图相关功能
    - 统一修改 qq-face 和 speech 插件的postinstall.js，改用package.json名称动态生成日志TAG
    - 补充各插件包简单README文件，介绍插件功能和用途
    - 通过postinstall脚本，自动为插件zip文件创建链接，提升插件管理便捷性

--- packages/create-erii/init.js
+++ packages/create-erii/init.js
@@ -2,24 +2,55 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
-const cwd = process.cwd();
-const pkgPath = path.join(cwd, 'package.json');
+function askProjectName() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-// Parse registry
-let registryArg = '';
-const regIdx = process.argv.indexOf('--registry');
-if (regIdx !== -1 && process.argv[regIdx + 1]) {
-  registryArg = ' --registry ' + process.argv[regIdx + 1];
+  return new Promise((resolve) => {
+    rl.question('Project name (erii): ', (answer) => {
+      rl.close();
+      resolve(answer.trim() || 'erii');
+    });
+  });
 }
 
-if (!fs.existsSync(pkgPath)) {
-  fs.writeFileSync(pkgPath, JSON.stringify({ private: true }, null, 2) + '\n');
+async function main() {
+  const projectName = await askProjectName();
+  const projectDir = path.join(process.cwd(), projectName);
+
+  if (fs.existsSync(projectDir)) {
+    console.error('Error: directory "' + projectName + '" already exists.');
+    process.exit(1);
+  }
+
+  fs.mkdirSync(projectDir);
+  console.log('Created directory ' + projectName);
+
+  const pkgPath = path.join(projectDir, 'package.json');
+  fs.writeFileSync(pkgPath, JSON.stringify({name: projectName, private: true}, null, 2) + '\n');
   console.log('Created package.json');
+
+  let registryArg = '';
+  const regIdx = process.argv.indexOf('--registry');
+  if (regIdx !== -1 && process.argv[regIdx + 1]) {
+    registryArg = ' --registry ' + process.argv[regIdx + 1];
+  }
+
+  const ownVersion = require('./package.json').version;
+  const pkg = '@spcookie/erii@^' + ownVersion;
+  console.log('Installing ' + pkg + '...');
+  execSync('npm install ' + pkg + registryArg, {cwd: projectDir, stdio: 'inherit'});
+
+  console.log('\nDone! Run the following to get started:\n');
+  console.log('  cd ' + projectName);
+  console.log('  npx erii');
 }
 
-// npm create foo@1.2.3 → downloads create-foo@1.2.3 → we install foo@^1.2.3
-const ownVersion = require('./package.json').version;
-const pkg = '@spcookie/erii@^' + ownVersion;
-console.log('Installing ' + pkg + '...');
-execSync('npm install ' + pkg + registryArg, { cwd, stdio: 'inherit' });
+main().catch((err) => {
+  console.error(err.message);
+  process.exit(1);
+});
